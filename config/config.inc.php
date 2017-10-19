f0f04f0ffc14021d057119d51ff1f5fd101a90b6
houqiang <zchouqiang@163.com>
2017-10-19 17:04:38 +0800

    数据库配置

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,19 +1,17 @@
 <?php
 if (!defined('SITE_PATH')) exit();
 return array(
-    // 数据库常用配置
-    'DB_TYPE'       => 'mysql',       // 数据库类型
+	// 数据库常用配置
+	'DB_TYPE'       => 'mysql',       // 数据库类型
 
-    'DB_HOST'   => 'demo-mysql.zhiyicx.com', //服务器地址
-    'DB_NAME'   => 'thinksns_v4', // 数据库名
-    'DB_USER'   => 'thinksns_v4', // 用户名
-    'DB_PWD'    => 'Xu0OeSUhr5bl',  // 密码
+	'DB_HOST'       => '47.94.239.57',    // 数据库服务器地址
+	'DB_NAME'       => 'thinksns',    // 数据库名
+	'DB_USER'       => 'aliyun',// 数据库用户名
+	'DB_PWD'        => 'aliyun',// 数据库密码
 
-    'DB_PORT'       => 3306,        // 数据库端口
-    'DB_PREFIX'     => 'ts_',// 数据库表前缀（因为漫游的原因，数据库表前缀必须写在本文件）
-    'DB_CHARSET'    => 'utf8',      // 数据库编码
-    'SECURE_CODE'   => '2353955a7c8910f676',  // 数据加密密钥
-    'COOKIE_PREFIX' => 'TS4_',      // # cookie
-
-    'exchange_type' => 5,  //智播积分兑换比例 20161030 bs
+	'DB_PORT'       => 3306,        // 数据库端口
+	'DB_PREFIX'     => 'ts_',// 数据库表前缀（因为漫游的原因，数据库表前缀必须写在本文件）
+	'DB_CHARSET'    => 'utf8',      // 数据库编码
+	'SECURE_CODE'   => '1596659e86a57eb40e',  // 数据加密密钥
+	'COOKIE_PREFIX' => 'TS4_',      // # cookie
 );
---
+++ storage/temp/datacache/6/6/TS4_660956d774b36f120b7d042b75bcd293.php
@@ -0,0 +1,4 @@
+<?php
+//000000000000a02a3c22aaccccd3bb5198c4efd0394ax^U±nÃ0Dÿ…{+¶‡ÛŽý„ˆc4òï%UgèÈwâéŽ„->
+ÞétI$žÐ)el|ÁGðŒNç„Ê5Èò:ÞnŠnÇuLAîK2¡Ua@¸Í‘Ïœ²¡NÑ!r¹ˆ‘^Io&¹Ø¼7·NÝ„²„H’„oÕî`Š†ISüÇ‡íÿ‘ŠX¦ðZ8ª N<çßÊì«³¬þÂZeæéþ»öÓa¹š¸Õ~ê§Ív›¯—³ë›¡kÚ¡«iõ$õtŸ?gZwßøç/2k-
+?>
